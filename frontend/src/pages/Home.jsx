2f6f997002d949ecd87436f56903fb26d7355ef4
Lius <22102950@usc.edu.ph>
2025-02-28 22:53:55 +0800

    Remove hardcoded footer from Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,13 +12,6 @@ function Home() {
       <Amenities />
       <Gallery />
       <Contact />
-      <footer className="bg-emerald-900 text-white py-8">
-        <div className="container mx-auto px-6 text-center">
-          <p className="text-white/90 font-light">
-            © 2024 Serenity Busay. All rights reserved.
-          </p>
-        </div>
-      </footer>
     </main>
   );
 }
